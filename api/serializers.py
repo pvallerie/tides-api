10fe91fbb8585e5e19b377c79a9d8e9834e8d4e5
pvallerie <petevallerie@gmail.com>
2021-06-03 16:09:04 -0400

    Add singup view and user serializers
    - added serializers file
    - added UserSignUpSerializer and UserSerializer
    - added SignUp view

---
+++ api/serializers.py
@@ -0,0 +1,33 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+# from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer to be used for User creation"""
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'email', 'password')
+        extra_kwargs = { 'password': { 'write_only': True, 'min_length': 5 } }
+    
+    # Method for User model creation
+    def create(self, validated_data):
+        return get_user_model().objects.create_user(**validated_data)
+
+class UserSignUpSerializer(serializers.Serializer):
+    """Serializer to be used for signing up"""
+    email = serializers.CharField(max_length=300, required=True)
+    password = serializers.CharField(required=True)
+    password_confirmation = serializers.CharField(required=True, write_only=True)
+
+    def validate(self, data):
+        # check for pw and pw confirmation
+        if not data['password'] or not data['password_confirmation']:
+            raise serializers.ValidationError('Please include a password and password confirmation')
+
+        # check whether pw and pw confirmation match
+        if data['password'] != data['password_confirmation']:
+            raise serializers.ValidationError('Please make sure your password and password confirmation match')
+
+        # if we pass checks, return data
+        return data
--- api/views.py
+++ api/views.py
@@ -1,6 +1,40 @@
+from django.contrib.auth import authenticate
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
+from rest_framework import status, generics
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .serializers import UserSerializer, UserSignUpSerializer
+from .models import User
 
 # test view
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+# Sign Up
+class SignUp(generics.CreateAPIView):
+    """View to sign up new User"""
+    # override auth/permission classes
+    authentication_classes = ()
+    permission_classes = ()
+
+    serializer_class = UserSignUpSerializer
+
+    def post(self, request):
+        # Validate request data using serializer
+        user = UserSignUpSerializer(data=request.data['credentials'])
+        # If data validated, create user
+        if user.is_valid():
+            created_user = UserSerializer(data=user.data)
+
+            if created_user.is_valid():
+                # Save user and send response
+                created_user.save()
+                return Response({ 'user': created_user.data }, status=status.HTTP_201_CREATED)
+            else:
+                return Response(created_user.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        else:
+            return Response(user.errors, status=status.HTTP_400_BAD_REQUEST)
