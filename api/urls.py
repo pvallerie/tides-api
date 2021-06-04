ac355663538805110143175730b5a1bc9f32ce4f
pvallerie <petevallerie@gmail.com>
2021-06-04 08:52:48 -0400

    Add signin url to app

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import index, SignUp
+from .views import index, SignUp, SignIn
 
 urlpatterns = [
     # test view
     path('', index, name='index'),
     # auth views
     path('sign-up/', SignUp.as_view(), name='sign-up'),
+    path('sign-in/', SignIn.as_view(), name='sign-in'),
 ]
